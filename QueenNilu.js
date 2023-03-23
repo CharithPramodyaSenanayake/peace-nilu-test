68215e97f7d0b755a87eede40658383966a8dc51
Queen NIlu <101912503+Dragonxjanith@users.noreply.github.com>
2023-03-23 09:25:13 +0530

    ddd

--- QueenNilu.js
+++ QueenNilu.js
@@ -9562,7 +9562,7 @@ QueenNilu.readMessages([m.key])
 
    // await QueenNilu.sendReadReceipt(from, m.sender, [m.key.id])
              await QueenNilu.sendPresenceUpdate('recording', m.chat) 
-             await QueenNilu.sendMessage(m.chat, { audio: {url :'https://github.com/Dragonxjanith/UPLOADS/raw/main/VOICE/Nilu.mp3'}, mimetype: 'audio/mp4', ptt: true }, { quoted: fakestatus }) 
+             await QueenNilu.sendMessage(m.chat, { audio: {url :'https://github.com/Dragonxjanith/UPLOADS/raw/main/VOICE/nilu.mp3'}, mimetype: 'audio/mp4', ptt: true }, { quoted: fakestatus }) 
  if (global.alive === 'default') {
  const ita = '```'
  await QueenNilu.sendMessage(from, { react: { text: `👋`, key: m.key }})
