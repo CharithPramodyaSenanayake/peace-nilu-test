9c53fc437b7f0882302f2e8ef60bfc0c77461195
Queen NIlu <101912503+Dragonxjanith@users.noreply.github.com>
2023-03-23 08:44:46 +0530

    fdd

--- QueenNilu.js
+++ QueenNilu.js
@@ -185,7 +185,6 @@ if (cek == null) return null
 	    const isBan = banned.includes(m.sender)
         const isPremium = premium.includes(m.sender)
         const isAutoSticker = m.isGroup ? autosticker.includes(from) : false
-        const Autoreply = m.isGroup ? autorep.includes(from) : false
         const solot = [
 		'🍊 : 🍒 : 🍐',
 		'🍒 : 🔔 : 🍊',
